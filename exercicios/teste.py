b1500d3f7f0a0636899fd40817e00c59ddb1bfcc
Matheus Silva <matheus.silva.t0130006@edu.atec.pt>
2025-10-15 06:42:47 -0700

    descobrir o maior ou menor

---
+++ exercicios/teste.py
@@ -0,0 +1,13 @@
+num1 = 0
+num2 = 0
+num3 = 0
+
+num1 = int(input("introduza número: "))
+num2 = int(input("introduza número: "))
+num3 = int(input("introduza número: "))
+
+maior = max(num1, num2, num3)
+menor = min(num1, num2, num3)
+    
+print("maior", maior)
+print("menor", menor)
